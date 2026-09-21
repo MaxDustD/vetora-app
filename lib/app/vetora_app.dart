e2a789625cbd9aea94215e8cd1766df0fa6ce491
Nilseo <patojubileu987@gmail.com>
2026-09-21 15:06:25 -0300

    feat: movimentação do código para pastas devidas em main e vetora_app

---
+++ lib/app/vetora_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../features/startup/presentation/splash_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vetora',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
+    );
+  }
+}
